0c94baf57d250eb7dff00c3a11b300a98cb69169
YuvarajPG <yuvar2978@gmail.com>
2025-10-14 23:29:53 +0530

    Refactor HTML structure and enhance CSS styles for Todo app; improve task input validation in JavaScript

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ let submit = document.getElementById("submit");
 let addTask = document.getElementById("addTask");
 let todoList = document.getElementById("todoList");
 
+let length = 0;
+
 submit.addEventListener("click", (event) => {
   let li = document.createElement("li");
-  if (`${addTask.value}` !== "") {
+  if (`${addTask.value}` == "") {
     li.innerText = `${addTask.value}`;
+    li.className = 'li'
+    li.setAttribute("contenteditable", "true");
     todoList.appendChild(li);
   } else {
     alert("enter a vaild value");
   }
   addTask.value = "";
 });
+
+
